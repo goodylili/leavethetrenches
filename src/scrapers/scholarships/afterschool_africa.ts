a36e8d60c93fe267e8f67b95af515eae0d7e85f2
goodylili <ukejegoodness599@gmail.com>
2026-02-20 10:05:05 +0100

    removed scraper to have standalone frontend

--- src/scrapers/scholarships/afterschool_africa.ts
+++
@@ -1,9 +0,0 @@
-import { Opportunity, SiteScraper } from "../types";
-
-const SOURCE_URL = "https://www.afterschoolafrica.com";
-
-export const scrapeAfterSchoolAfricaScholarships: SiteScraper = async () => {
-  const opportunities: Opportunity[] = [];
-
-  return opportunities;
-};
